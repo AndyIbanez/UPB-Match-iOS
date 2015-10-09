59cbaa9447f83c3ef48779aa78054c3f8354850c
Andy Ibanez <andy@ibanez.me>
2015-10-09 10:34:05 -0400

    Initial Commit

---
+++ UPB MatchTests/UPB_MatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  UPB_MatchTests.swift
+//  UPB MatchTests
+//
+//  Created by Andy Ibanez on 10/9/15.
+//  Copyright © 2015 Fairese. All rights reserved.
+//
+
+import XCTest
+@testable import UPB_Match
+
+class UPB_MatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
