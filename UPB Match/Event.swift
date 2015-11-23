83ce1b2b08ee3dee8a5cc6ec8a6be338336b9d09
Andy Ibanez <andy@andyibanez.com>
2015-11-23 18:02:56 -0400

    Event class done.

---
+++ UPB Match/Event.swift
@@ -0,0 +1,40 @@
+//
+//  Event.swift
+//  UPB Match
+//
+//  Created by Andy Ibanez on 11/23/15.
+//  Copyright © 2015 Fairese. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a single event.
+public class Event {
+    /// Event ID.
+    let ID: String
+    
+    /// Event name.
+    let name: String
+    
+    /// Event day.
+    let day: Int
+    
+    /// Event hour.
+    let hour: String
+    
+    /// Event Date.
+    let date: NSDate
+    
+    /// Event description.
+    let description: String
+    
+    /// Creates a new Event.
+    init(id: String, name: String, day: Int, hour: String, date: NSDate, description: String) {
+        self.ID = id
+        self.name = name
+        self.day = day
+        self.hour = hour
+        self.date = date
+        self.description = description
+    }
+}
